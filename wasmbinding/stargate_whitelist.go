7bdd954f1825c9c2f27fe3c297025ed29b8c2187
Gabe Rodriguez <grod220@gmail.com>
2023-03-23 13:26:45 +0100

    bump version

--- wasmbinding/stargate_whitelist.go
+++ wasmbinding/stargate_whitelist.go
@@ -14,7 +14,7 @@ import (
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	ibctransfertypes "github.com/cosmos/ibc-go/v4/modules/apps/transfer/types"
 
-	conentratedtypes "github.com/osmosis-labs/osmosis/v13/x/concentrated-liquidity/types"
+	conentratedtypes "github.com/osmosis-labs/osmosis/v15/x/concentrated-liquidity/types"
 	downtimequerytypes "github.com/osmosis-labs/osmosis/v15/x/downtime-detector/client/queryproto"
 	gammtypes "github.com/osmosis-labs/osmosis/v15/x/gamm/types"
 	gammv2types "github.com/osmosis-labs/osmosis/v15/x/gamm/v2types"
